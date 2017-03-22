afedfb3218f0a8ba316e36a38e0ede6df594307e
Alexandre Gee <71alexgee@gmail.com>
2017-03-21 20:56:03 -0400

    Update efuse.py

--- efuse.py
+++ efuse.py
@@ -1,1 +1,3 @@
 #Currently in progress. For E-Fuse bit reprogramming. (Multiple devices/address conflicts)
+#Build the schematic circuit to program it easily, since the efuse programming bit requires
+#a pin to be pulled high or low at certain intervals
